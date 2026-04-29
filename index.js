b8254b7b951b676b8d8b8bb346f267fa73471530
Sean Crosby <crosbyse@byui.edu>
2026-04-29 14:40:06 -0600

    adding joke

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('<html><body><h1>Hello World!</h1></body></html>');
+});
+app.get('/joke', (req, res) => {
+  res.send('<html><body>!false — It’s funny because it’s true.</body></html>');
 });
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
